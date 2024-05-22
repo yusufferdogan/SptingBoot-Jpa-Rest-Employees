a1ff50663f6ab1e9b4800b1b88f0f028f27e8860
yusufferdogan <yusuffx03@gmail.com>
2024-05-22 14:37:12 +0300

    Initial commit

---
+++ src/test/java/com/yusuf/erdogan/jparestemployee/JpaRestEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusuf.erdogan.jparestemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRestEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
